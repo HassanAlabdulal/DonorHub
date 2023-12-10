0d1c78adc50e0bdee4f3fe015f3b9b70b9c81394
HassanAlabdulal <hhhalabdulaal@gmail.com>
2023-12-10 16:05:29 +0300

    Add notifications section in menu profile

--- src/components/UI/ProfileMenu.tsx
+++ src/components/UI/ProfileMenu.tsx
@@ -79,6 +79,32 @@ const ProfileMenu: React.FC<ProfileMenuProps> = ({ toggled }) => {
               </Typography>
             </a>
           </MenuItem>
+          <MenuItem className="flex items-center gap-2">
+            <a
+              href="NotificationPage"
+              className="flex items-center gap-2 text-left"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                width="16"
+                height="16"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0"
+                />
+              </svg>
+
+              <Typography variant="small" className="font-medium">
+                Notifications
+              </Typography>
+            </a>
+          </MenuItem>
 
           <hr className="my-2 border-blue-gray-50" />
           <MenuItem className="flex items-center gap-2 ">
